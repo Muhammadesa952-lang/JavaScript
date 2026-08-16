014df6cc440b9de776fc57e4532de796cd62adf0
MuhammadEsa-dev <muhammadesa952@gmail.com>
2026-08-17 01:07:54 +0500

    file4.js

---
+++ file4.js
@@ -0,0 +1,17 @@
+let name = "muhammad";
+console.log(name);
+let age = "34";
+console.log(age);
+
+const PI = 3.14;
+console.log(PI);
+
+{
+  let a = 10;
+  console.log(a);
+}
+
+{
+  let a = 5;
+  console.log(a);
+}

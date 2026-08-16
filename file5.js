998730033f1fa272c12177279ff300d26ac84f11
MuhammadEsa-dev <muhammadesa952@gmail.com>
2026-08-17 01:08:48 +0500

    file5.js

---
+++ file5.js
@@ -0,0 +1,18 @@
+let price = 100.78;
+let fullName = "Tony stark";
+isFollow = true;
+
+let x = null;
+
+let p = BigInt("1234");
+
+let y = Symbol("Hello!");
+
+const Student = {
+  name: "Abubakkar",
+  age: 19,
+  Marks: 1073,
+  ispass: true,
+};
+Student["age"] = Student["age"] + 6;
+console.log(Student["age"]);

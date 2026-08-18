d48378f0222cf518024e27baa145e7e17dcbf541
MuhammadEsa-dev <muhammadesa952@gmail.com>
2026-08-18 23:14:01 +0500

    file6.js

---
+++ file6.js
@@ -0,0 +1,22 @@
+const product = {
+  title: "Ball pen",
+  rating: 4,
+  offer: 5,
+  price: 270,
+};
+
+console.log(product);
+
+const profile = {
+  username: "@space_for_code",
+  followers: 569,
+  isFollow: false,
+  following: 4,
+};
+
+console.log(profile);
+
+console.log(typeof profile["username"]);
+console.log(typeof profile["isFollow"]);
+console.log(typeof profile["followers"]);
+console.log(typeof profile["following"]);

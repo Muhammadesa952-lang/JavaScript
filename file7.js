db67575d79fcfff91b1c1807cd2753ef5dcc9d9b
MuhammadEsa-dev <muhammadesa952@gmail.com>
2026-08-20 23:50:54 +0500

    file7.js

---
+++ file7.js
@@ -0,0 +1,37 @@
+//Comments in javascript
+console.log("Hello world!");
+//this is a comment
+/* this is a multi line comment which is print on our window of laotop */
+
+// Arithimetic Operators
+let a = 6;
+let b = 9;
+
+let c = a + b;
+let d = a - b;
+let e = a * b;
+let f = a / b;
+let g = a % b;
+console.log("a=", a, "b=", b);
+console.log("a + b=", c);
+console.log("a - b=", d);
+console.log("a * b=", e);
+console.log("a / b=", f);
+console.log("a % b=", g);
+
+let m = 5;
+let n = 2;
+console.log("m%n=", m % n);
+
+console.log("m**n=", m ** n); //5^2=25 it should be the answer of the  power.
+
+//Unary Operator
+a = a + 1;
+console.log("a=", a); //it simply write in unary operator as:
+a++;
+console.log("a=", a);
+
+b = b - 1;
+console.log("b=", b); //it simply write in unary operator as:
+b--;
+console.log("b=", b);
